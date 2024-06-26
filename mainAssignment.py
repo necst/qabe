730afded33ac72d61ffab09fa01329914cb5e917
Gianluca Scanu <103889787+GianlucaScanu@users.noreply.github.com>
2024-06-26 14:24:17 +0000

    final matrix for q. assignm.

--- mainAssignment.py
+++ mainAssignment.py
@@ -1,5 +1,6 @@
 import quadraticAssignment
 
-list = [[0,1,3],[4,5,6],[6,5,4]]
-problem = quadraticAssignment.QuadraticAssignmentProblem(list,None,5)
+list = [[0,5,2],[5,0,3],[2,3,0]]
+list2 = [[0,8,15],[8,0,13],[15,13,0]]
+problem = quadraticAssignment.QuadraticAssignmentProblem(list,list2,200)
 problem.prepare()
--- quadraticAssignment.py
+++ quadraticAssignment.py
@@ -4,6 +4,61 @@ from dwave.embedding.chain_strength import *
 from dwave.system import DWaveSampler
 from dwave.system import LeapHybridSampler
 from collections import defaultdict 
+import math
+import re
+
+
+
+def modified_squared_pol(coeff,offset):
+
+  n = len(coeff) - 1
+  squared_coeff = {}
+
+  #squared terms(a_i^2)
+  for i in range(n):
+    index = i*offset
+    squared_coeff[f"x{index+1}@2"] = coeff[i] * coeff[i]
+
+  #cross terms(a_i * a_j)
+  for i in range(n):
+    for j in range(i + 1, n):
+      index_i = i*offset
+      index_j = j*offset
+      squared_coeff[f"x{1+index_i}x{1+index_j}"] = 2 * coeff[i] * coeff[j]
+
+  #costant multiplying terms (a_i * c)
+  for i in range(n):
+    index = i*offset
+    squared_coeff[f"x{index+1}c"] = 2 * coeff[i] * coeff[-1]
+
+  #constant squared (c^2)
+  squared_coeff["c^2"] = coeff[-1] ** 2
+
+  return squared_coeff
+
+
+def squared_pol(coeff):
+
+  n = len(coeff) - 1
+  squared_coeff = {}
+
+  #squared terms(a_i^2)
+  for i in range(n):
+    squared_coeff[f"x{i+1}@2"] = coeff[i] * coeff[i]
+
+  #cross terms(a_i * a_j)
+  for i in range(n):
+    for j in range(i + 1, n):
+      squared_coeff[f"x{i+1}x{j+1}"] = 2 * coeff[i] * coeff[j]
+
+  #costant multiplying terms (a_i * c)
+  for i in range(n):
+    squared_coeff[f"x{i+1}c"] = 2 * coeff[i] * coeff[-1]
+
+  #constant squared (c^2)
+  squared_coeff["c^2"] = coeff[-1] ** 2
+
+  return squared_coeff
 
 
 class QuadraticAssignmentProblem:
@@ -15,7 +70,7 @@ class QuadraticAssignmentProblem:
     of the facilities to locations to minimize the weighted
     flow across the system.
     """
-    def __init__(self, facilities, locations, penalty = 10):
+    def __init__(self, facilities, distances, penalty = 10):
         """
         Constructor of the graph Coloring class.
         Params:
@@ -24,14 +79,14 @@ class QuadraticAssignmentProblem:
                 facilities in the form of a list of lists
                 where facilities[i][j] represents the flow
                 between facility i and facility j
-        locations: matrix indicating the distances between
+        distances: matrix indicating the distances between
                 locations in the form of a list of lists
                 where locations[i][j] represents the distance
                 between location i and location j
         """
 
         self.f = facilities
-        self.l = locations
+        self.d = distances
         self.pen = penalty
         self.q = defaultdict(int)
 
@@ -40,4 +95,113 @@ class QuadraticAssignmentProblem:
 
         var_number = len(self.f)
 
+        squared_var_number = var_number*var_number
+
         print(var_number)
+
+        for k in range(1,squared_var_number):
+
+            for s in range(k+1,squared_var_number+1):
+
+                #from 1 to n
+                i = math.floor((k-1) / var_number) + 1
+                j = (k-1) % var_number + 1
+
+                #from 1 to n
+                m = math.floor((s-1)/var_number) + 1
+                n = (s-1)%var_number + 1
+
+    
+                if (i != m and j != n):
+                    print("k: %s, s: %s, i: %s, j: %s, m: %s, n: %s" % (k,s,i,j,m,n))
+                    # Using x-1 to access matrices from 0 to n
+                    self.q[(k-1,s-1)] = self.q[(k-1,s-1)] + 2*self.f[i-1][m-1]*self.d[j-1][n-1]
+
+        print(self.q)
+
+        pol = []
+
+        for i in range(var_number):
+            pol.append(1)
+
+        pol.append(-1)
+
+        w_dict = squared_pol(pol)
+
+        for key in w_dict.keys():
+           w_dict[key] = self.pen * w_dict[key]
+
+        for i in range(0,squared_var_number,var_number):
+            print(i)
+
+            for term,coefficient in w_dict.items():
+                if (re.match(r'x(\d+)x(\d+)',term)):
+                    match = re.match(r'x(\d+)x(\d+)',term)
+                    row = int(match.group(1)) - 1 + i
+                    col = int(match.group(2)) - 1 + i
+
+                    #adding the corresponding term to the Q matrix
+                    if (col >= row):
+                        self.q[(row,col)] = self.q[(row,col)] + coefficient
+                        self.q[(col,row)] = self.q[(col,row)] + coefficient
+
+                elif (re.match(r'x(\d+)@2', term)):
+                    match = re.match(r'x(\d+)@2',term)
+                    index = int(match.group(1)) - 1 + i
+                        
+                    #adding the corresponding term to the Q matrix
+                    self.q[(index,index)] = self.q[(index,index)] + coefficient
+
+                elif (re.match(r'x(\d+)c', term)):      
+                    match = re.match(r'x(\d+)c',term)
+                    index = int(match.group(1)) - 1 + i
+                        
+                    #adding the corresponding term to the Q matrix
+                    self.q[(index,index)] = self.q[(index,index)] + coefficient
+
+        print(self.q)
+
+
+        pol = []
+
+        for i in range(var_number):
+            pol.append(1)
+
+        pol.append(-1)
+
+        w_dict = modified_squared_pol(pol,var_number)
+
+        for key in w_dict.keys():
+           w_dict[key] = self.pen * w_dict[key]
+
+        for i in range(0,var_number):
+              for term,coefficient in w_dict.items():
+                if (re.match(r'x(\d+)x(\d+)',term)):
+                    match = re.match(r'x(\d+)x(\d+)',term)
+                    row = int(match.group(1)) - 1 + i
+                    col = int(match.group(2)) - 1 + i
+
+                    #adding the corresponding term to the Q matrix
+                    if (col >= row):
+                        self.q[(row,col)] = self.q[(row,col)] + coefficient
+                        self.q[(col,row)] = self.q[(col,row)] + coefficient
+                        print("ho aggiunto su %s,%s" % (row,col))
+
+                elif (re.match(r'x(\d+)@2', term)):
+                    match = re.match(r'x(\d+)@2',term)
+                    index = int(match.group(1)) - 1 + i
+                        
+                    #adding the corresponding term to the Q matrix
+                    self.q[(index,index)] = self.q[(index,index)] + coefficient
+                    print("ho aggiunto su %s,%s" % (index,index))
+
+                elif (re.match(r'x(\d+)c', term)):      
+                    match = re.match(r'x(\d+)c',term)
+                    index = int(match.group(1)) - 1 + i
+                        
+                    #adding the corresponding term to the Q matrix
+                    self.q[(index,index)] = self.q[(index,index)] + coefficient
+                    print("ho aggiunto su %s,%s" % (index,index))
+
+        print(self.q)
+
